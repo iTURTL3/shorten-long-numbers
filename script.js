3f03919168f3ac193084c9aa99f4553562150779
jakemadness <j4kem4dness@live.com>
2018-04-17 14:17:33 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+var shortenNumber = function(number, places, force) {
+   var suffix = '';
+   if ( number >= 1e15 ) {
+      number /= 1e15;
+      suffix  = 'Q';
+   }
+   else if ( number >= 1e12 ) {
+      number /= 1e12;
+      suffix  = 'T';
+   }
+   else if ( number >= 1e9 ) {
+      number /= 1e9;
+      suffix  = 'B';
+   }
+   else if ( number >= 1e6 ) {
+      number /= 1e6;
+      suffix  = 'M';
+   }
+   else if ( number >= 1e3 ) {
+      number /= 1e3;
+      suffix  = 'K';
+   }
+   return (force ? String : Number)(number.toFixed(places)) + suffix;
+};
